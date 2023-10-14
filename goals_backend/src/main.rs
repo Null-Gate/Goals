2fb591d0764372a8cc12a7c39e2d36cd4ce6a5f0
Linus Walker <w000alker@gmail.com>
2023-10-14 20:16:58 +0630

    (feat) Done for tonight

--- goals_backend/src/main.rs
+++ goals_backend/src/main.rs
@@ -1,5 +1,14 @@
 use actix_web::{HttpServer, App, get, HttpResponse, Scope, web::{self, Json}};
+use surrealdb::{Surreal, engine::local::{File, Db}};
+use async_once::AsyncOnce;
+use lazy_static::lazy_static;
 use serde::{Serialize, Deserialize};
+
+lazy_static! {
+    static ref DB: AsyncOnce<Surreal<Db>> = AsyncOnce::new(async {
+        Surreal::new::<File>("db.db").await.unwrap()
+    });
+}
 
 #[derive(Serialize, Deserialize)]
 struct LoginInfo {
