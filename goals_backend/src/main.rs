a947d1ef8082415bd0c90b868090958a7ea471c8
Linus Walker <w000alker@gmail.com>
2023-10-14 17:35:50 +0630

    (feat) Started cooking for backend

--- goals_backend/src/main.rs
+++ goals_backend/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, App, get, HttpResponse, Scope, web::{self, Json}};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct LoginInfo {
+    username: String,
+    fullname: String,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(scope())).bind(("127.0.0.1", 9899))?.run().await
+}
+
+fn scope() -> Scope {
+    web::scope("/api").service(login)
+}
+
+#[get("/login")]
+async fn login(info: Json<LoginInfo>) -> HttpResponse {
+    todo!()
 }
