c59431c7b50fa44177c275a2a6748076bf94ab02
Linus Walker <w000alker@gmail.com>
2023-10-23 14:35:42 +0630

    (feat) working for login

--- goals_backend/src/main.rs
+++ goals_backend/src/main.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 
-use jsonwebtoken;
+use argon2::verify_encoded;
+use chrono::{Utc, Duration};
 use actix_web::{HttpServer, App, get, HttpResponse, Scope, web::{self, Json}};
-use surrealdb::{Surreal, engine::local::{File, Db}, opt::QueryResult};
+use jsonwebtoken::{encode, Header, EncodingKey};
+use surrealdb::{Surreal, engine::local::{File, Db}};
 use async_once::AsyncOnce;
 use lazy_static::lazy_static;
 use serde::{Serialize, Deserialize};
@@ -11,6 +13,13 @@ lazy_static! {
     static ref DB: AsyncOnce<Surreal<Db>> = AsyncOnce::new(async {
         Surreal::new::<File>("db.db").await.unwrap()
     });
+}
+
+#[derive(Serialize, Deserialize)]
+struct Claims {
+    username: String,
+    password: String,
+    exp: usize,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -86,9 +95,27 @@ async fn login(info: Json<LoginInfo>) -> HttpResponse {
     let sql = r#"SELECT username, fullname, password "#;
     match db.query(sql).await {
         Ok(mut user_check) => {
-            if let Ok(Some(user)) = user_check.take::<Option<String>>("password") {
+            if let Ok(Some(pass)) = user_check.take::<Option<String>>("password") {
+                match verify_encoded(&pass, info.password.as_bytes()) {
+                    Ok(status) => {
+                        if !status {
+                            HttpResponse::InternalServerError().json(Resp::new("Sorry Password didn't matched!"))
+                        } else {
+                            let exp = (Utc::now() + Duration::days(9999999)).timestamp() as usize;
+                            let claims = Claims {
+                                username: info.username.clone(),
+                                password: info.password.clone(),
+                                exp
+                            };
+                            let jwt = encode(&Header::default(), &claims, &EncodingKey::from_secret(todo!()));
+                            todo!()
+                        }
+                    },
+                    Err(_) => HttpResponse::InternalServerError().json(Resp::new("Sorry We are having some problem when checking your password!"))
+                }
+            } else {
+                HttpResponse::NotFound().json(Resp::new(&format!("Sorry No User is founded with username of: {}", info.username)))
             }
-            HttpResponse::NotFound().json(Resp::new(&format!("Sorry No User is founded with username of: {}", info.username)))
         },
         Err(_) => {
             HttpResponse::InternalServerError().json(Resp::new("Sorry We are some problem in opening database!!"))
