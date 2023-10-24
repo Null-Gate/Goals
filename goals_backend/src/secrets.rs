aceec504a722355f76dac83971067116840d8db3
Linus Walker <w000alker@gmail.com>
2023-10-24 15:32:47 +0630

    fuckit

--- goals_backend/src/secrets.rs
+++ goals_backend/src/secrets.rs
@@ -1,1 +1,1 @@
-pub const JWT_SECRETS: &str = r#"add_here_jwt_secret_key"#;
+pub const JWT_SECRETS: &str = r#";c5hve;Sla]a}Ur[yr=D/AJLy;;bht$^Z(9;7L.ogw>]ke9S(jkshfkh>?><>>>>>SD+---==-=-=-)(*&&^^%%%$$$####@@!!#%^&&*^*%$^^&$$skllhvkbxckbbsdfbsbksbbjs3423478378676//dsfsd#;SDDFefds:"#;
