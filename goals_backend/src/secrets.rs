834153d7e919190b1c6521a5fe3ddfea0d861a57
Linus Walker <85013114+Walker-00@users.noreply.github.com>
2023-10-24 15:37:21 +0630

    Delete goals_backend/src/secrets.rs

--- goals_backend/src/secrets.rs
+++
@@ -1,1 +0,0 @@
-pub const JWT_SECRETS: &str = r#";c5hve;Sla]a}Ur[yr=D/AJLy;;bht$^Z(9;7L.ogw>]ke9S(jkshfkh>?><>>>>>SD+---==-=-=-)(*&&^^%%%$$$####@@!!#%^&&*^*%$^^&$$skllhvkbxckbbsdfbsbksbbjs3423478378676//dsfsd#;SDDFefds:"#;
